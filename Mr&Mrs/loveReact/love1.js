f2bbcbc6d5ebf0303cf5a14e567000aa591a6185
techsubhajit <dummysubhajit@gmail.com>
2023-06-23 01:21:52 +0530

    first

---
+++ Mr&Mrs/loveReact/love1.js
@@ -0,0 +1,47 @@
+let lovesIcons = document.querySelectorAll(".loveIcons");
+
+lovesIcons.forEach((loveIcon) => {
+  loveIcon.addEventListener("click", () => {
+    let count = 0;
+
+    let intervel = setInterval(() => {
+      loveIcon.classList.add("bg_hearts");
+      loveIcon.classList.remove("fa-heart");
+
+      count++;
+
+      if (count > 20) {
+        loveIcon.classList.remove("bg_hearts");
+        loveIcon.classList.add("fa-heart");
+        loveIcon.innerHTML = "";
+
+        // clearInterval(love);
+        clearInterval(intervel);
+      }
+    }, 300);
+  });
+});
+
+var love = setInterval(function () {
+  var r_num = Math.floor(Math.random() * 40) + 1;
+  var r_size = Math.floor(Math.random() * 15) + 5;
+  var r_left = Math.floor(Math.random() * 100) + 1;
+  var r_bg = Math.floor(Math.random() * 25) + 100;
+  var r_time = Math.floor(Math.random() * 5) + 5;
+
+  $(".bg_hearts").append("<div class='heart' style='width:" + r_size + "px;height:" + r_size + "px;left:" + r_left + "%;background:rgba(255," + (r_bg - 25) + "," + r_bg + ",1);-webkit-animation:love " + r_time + "s ease;-moz-animation:love " + r_time + "s ease;-ms-animation:love " + r_time + "s ease;animation:love " + r_time + "s ease'></div>");
+
+  $(".bg_hearts").append("<div class='heart' style='width:" + (r_size - 10) + "px;height:" + (r_size - 10) + "px;left:" + (r_left + r_num) + "%;background:rgba(255," + (r_bg - 25) + "," + (r_bg + 25) + ",1);-webkit-animation:love " + (r_time + 5) + "s ease;-moz-animation:love " + (r_time + 5) + "s ease;-ms-animation:love " + (r_time + 5) + "s ease;animation:love " + (r_time + 5) + "s ease'></div>");
+
+  $(".heart").each(function () {
+    var top = $(this)
+      .css("top")
+      .replace(/[^-\d\.]/g, "");
+    var width = $(this)
+      .css("width")
+      .replace(/[^-\d\.]/g, "");
+    if (top <= -100 || width >= 150) {
+      $(this).detach();
+    }
+  });
+}, 500);
